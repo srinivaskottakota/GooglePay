42d53817466d8154026f91a6ac00f8bea79d60ec
Srinivas Kottakota <139893236+srinivaskottakota@users.noreply.github.com>
2024-06-01 14:42:35 +0530

    Add files via upload

---
+++ BankAccount.py
@@ -0,0 +1,5 @@
+class BankAccount:
+    def __init__(self):
+        self.bankName = None 
+        self.balance = 0 
+ 
---
+++ User.py
@@ -0,0 +1,7 @@
+class User:
+    def __init__(self):
+        self.fullName = None 
+        self.mobileNumber = None 
+        self.pin = None 
+        self.previousTransactions = []
+        self.linkedBankAccounts = []
